ad9585e0fc0e95863f3d879fe99c0aad28cb1083
Sherif Shoukry <Shoukrysherif5@gmail.com>
2025-04-11 20:46:17 +0200

    update the profile page

--- edit.js
+++ edit.js
@@ -4,21 +4,25 @@ if(localStorage.getItem("photo")==null || localStorage.getItem("photo")=="") {
 }else {
     editImage.innerHTML = `<img src="imgs/${localStorage.getItem("photo")}">`;
 }
+
 //check the new first name ,last name and phone number is correct or  not
 let fnameInput = document.getElementById("fname");
 let lnameInput = document.getElementById("lname");
 let nameFormat = /^([A-Za-z]{3,})$/;
 let phoneInput = document.getElementById("phone");
 let phoneFormat = /^([0-9]{11})$/;
+
 //error message
 let fnameError = document.getElementById("fname-error");
 let lnameError = document.getElementById("lname-error");
 let phoneError = document.getElementById("phone-error");
+
 //read the current data from localstorage to update
 document.getElementById("fname").value = localStorage.getItem("fname");
 document.getElementById("lname").value = localStorage.getItem("lname");
 document.getElementById("phone").value = localStorage.getItem("phone");
 document.getElementById("pass").value = localStorage.getItem("pass");
+
 //check the first name
 fnameInput.onblur = () => {
     if(nameFormat.test(fnameInput.value)){
@@ -29,6 +33,7 @@ fnameInput.onblur = () => {
         fnameInput.style.border = "3px solid red";
     }
 }
+
 //check the last name
 lnameInput.onblur = () => {
     if(nameFormat.test(lnameInput.value)){
@@ -39,6 +44,7 @@ lnameInput.onblur = () => {
         lnameInput.style.border = "3px solid red";
     }
 }
+
 //check the phone number
 phoneInput.onblur = () => {
     if(phoneFormat.test(phoneInput.value)){
@@ -63,18 +69,21 @@ form.onsubmit = () => {
 
 // set the new values of inputs into localstorage and read from it
 window.addEventListener("DOMContentLoaded",() => {
+    
     let url = new URLSearchParams(location.search);
     if(url.get("fname")!=null) {
         localStorage.setItem("fname",url.get("fname"));
         localStorage.setItem("lname",url.get("lname"));
         localStorage.setItem("phone",url.get("phone"));
-        localStorage.setItem("pass",url.get("password"));
+        localStorage.setItem("password",url.get("password"));
         url.get("photo")==""? "":localStorage.setItem("photo",url.get("photo"));
     }
+    
     document.getElementById("fname").value = localStorage.getItem("fname");
     document.getElementById("lname").value = localStorage.getItem("lname");
     document.getElementById("phone").value = localStorage.getItem("phone");
-    document.getElementById("pass").value = localStorage.getItem("pass");
+    document.getElementById("password").value = localStorage.getItem("password");
+
     if(localStorage.getItem("photo")==null || localStorage.getItem("photo")=="") {
         editImage.innerHTML = `<i class="fa-solid fa-user user"></i>`;
     }else {
--- main.js
+++ main.js
@@ -1,136 +1,149 @@
-//Profile Data 
-localStorage.setItem("fname","Sherif");
-localStorage.setItem("lname","Shoukry");
-localStorage.setItem("email","");
-localStorage.setItem("pass","5566");
-localStorage.setItem("phone","01110317344");
-localStorage.setItem("country","");
-localStorage.setItem("photo","photo.jpg");
-
-const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+//Joined Date
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 let date = new Date();
 let month = months[date.getMonth()];
+localStorage.setItem("Joined_Date", month + " " + date.getFullYear());
 
-localStorage.setItem("Joined_Date",month+" "+date.getFullYear());
+//Api Requst
+let moviesData = new XMLHttpRequest;
+moviesData.open("GET","https://api.themoviedb.org/3/trending/all/week?api_key=eb2ea9d1a56bfee6c00b801429fd0d16");
+moviesData.onreadystatechange = () => {
+  if(moviesData.readyState == 4 && moviesData.status == 200) {
+    let moviesDataRes = moviesData.response;
+    let moviesDataResObj = JSON.parse(moviesDataRes);
+
+    //movie ID
+    let moveieId = JSON.parse(localStorage.getItem("favorites_id"));
+
+    //check if the favorites array is empty or not
+    if(moveieId.length != 0) {
+      let favoriteArray =[];
+      for (const favMovie of moveieId) {
+        for (const element of moviesDataResObj.results) {
+          if(favMovie == element.id) {
+            favoriteArray.push({
+              id:element.id,
+              name: (element.name || element.title),
+              src:element.poster_path,
+            });
+          }
+        }
+      }
+      localStorage.setItem("favorites",JSON.stringify(favoriteArray));
+    }
+  }
+}
+moviesData.send();
 
 // get full name , Joined Date and photo from localstorage
 let photo = document.getElementById("profile-img");
 let name = document.getElementById("name");
 let joinedDate = document.getElementById("joined-date");
-if(localStorage.getItem("photo")==null || localStorage.getItem("photo")=="") {
-    photo.innerHTML = `<i class="fa-solid fa-user user"></i>`;
-
-}else {
-    photo.innerHTML = `<img src="imgs/${localStorage.getItem("photo")}">`;
+if (
+  localStorage.getItem("photo") == null ||
+  localStorage.getItem("photo") == ""
+) {
+  photo.innerHTML = `<i class="fa-solid fa-user user"></i>`;
+} else {
+  photo.innerHTML = `<img src="imgs/${localStorage.getItem("photo")}">`;
 }
-let fullName = `${localStorage.getItem("fname")} ${localStorage.getItem("lname")}`
+let fullName = `${localStorage.getItem("fname")} ${localStorage.getItem(
+  "lname"
+)}`;
 name.innerHTML = fullName;
-joinedDate.innerHTML = `<i class="fa-solid fa-calendar-days"></i> ${localStorage.getItem("Joined_Date")}`;
-
-
-// data of watching list 
-let watchlistdataObject = [
-    {
-    id:1,
-    src:'imgs/Bogeyman.jpg',
-    name:'Bogeyman'
-    },
-    {
-    id:2,
-    src:'imgs/Venom.jpg',
-    name:'Venom'
-    },
-    {
-    id:3,
-    src:'imgs/Robyhouse.jpg',
-    name:'Robyhouse'
-    },
-    {
-    id:4,
-    src:'imgs/Alexandria.jpg',
-    name:'Alexandria'
-    },
-];
-let favoritesdataObject = [
-    {
-    id:1,
-    src:'imgs/Venom.jpg',
-    name:'Venom'
-    },
-    {
-    id:2,
-    src:'imgs/Robyhouse.jpg',
-    name:'Robyhouse'
-    },
-    {
-    id:3,
-    src:'imgs/Alexandria.jpg',
-    name:'Alexandria'
-    },
-];
-localStorage.setItem("watchlist",JSON.stringify(watchlistdataObject));
-localStorage.setItem("favorites",JSON.stringify(favoritesdataObject));
+joinedDate.innerHTML = `<i class="fa-solid fa-calendar-days"></i> ${localStorage.getItem(
+  "Joined_Date"
+)}`;
 
 //Display The Count of Videos in Watching list and favorite list & watchlist and favorites in head
-let watchlistCounter =document.getElementById("count-of-watchlist");
-let favoritelistCounter =document.getElementById("count-of-favoritelist");
-let watchlistCounterHead =document.getElementById("watchlist-head");
-let favoriteCounterHead =document.getElementById("favorites-head");
-function counterFun(str,obj,obj2) {
-    let listObj = JSON.parse(localStorage.getItem(str));
-    obj.innerHTML = listObj.length;
-    obj2.innerHTML = listObj.length;
+let watchlistCounter = document.getElementById("count-of-watchlist");
+let favoritelistCounter = document.getElementById("count-of-favoritelist");
+let watchlistCounterHead = document.getElementById("watchlist-head");
+let favoriteCounterHead = document.getElementById("favorites-head");
+
+function counterFun(localStorageKey) {
+  let listObj = JSON.parse(localStorage.getItem(localStorageKey));
+  return listObj.length;
 };
-if(JSON.parse(localStorage.getItem("watchlist"))!= null){
-    counterFun("watchlist",watchlistCounter,watchlistCounterHead);
+
+if (JSON.parse(localStorage.getItem("watchlist")) != null) {
+  watchlistCounter.innerHTML = counterFun("watchlist");
+  watchlistCounterHead.innerHTML = counterFun("watchlist");
 }
-if(JSON.parse(localStorage.getItem("favorites"))!= null){
-    counterFun("favorites",favoritelistCounter,favoriteCounterHead);
+if (JSON.parse(localStorage.getItem("favorites")) != null) {
+  favoritelistCounter.innerHTML = counterFun("favorites");
+  favoriteCounterHead.innerHTML = counterFun("favorites");
 }
 
 //Display Videos from local storage into Watch list and favorites list
 let watchlistVideos = document.getElementById("watchlist-videos");
 let favoritelistVideos = document.getElementById("favoritelist-videos");
-function displayFromLocalStorage(str,box){
-    let listObj = JSON.parse(localStorage.getItem(str));
-    if(listObj!= null && listObj.length!=0){
-        box.classList.add("grid");
-        for (const element of listObj) {
-            let video = `<div class="video">
+
+function displayFromLocalStorage(localStorageKey,htmlBox) {
+  let listObj = JSON.parse(localStorage.getItem(localStorageKey));
+  if (listObj != null && listObj.length != 0) {
+    htmlBox.classList.add("grid");
+    for (const element of listObj) {
+      let video = `<div class="video">
             <a href="#">
-                <img src="${element.src}" >
+                <img src="https://image.tmdb.org/t/p/w500/${element.src}" >
                 <span>${element.name}</span>
             </a>
         </div>`;
-        box.innerHTML += video;
-        };
-    }else {
-        box.classList.remove("grid");
-        let emptyMessage = `<div class="empty">
+        htmlBox.innerHTML += video;
+    }
+  } else {
+    htmlBox.classList.remove("grid");
+    let emptyMessage = `<div class="empty">
         <p>No Watchlist Yet</p>
         <a href="#">Browse Popular Movies</a>
     </div>`;
-    box.innerHTML = emptyMessage;
-    }
+    htmlBox.innerHTML = emptyMessage;
+  }
 }
-displayFromLocalStorage("watchlist",watchlistVideos);
-displayFromLocalStorage("favorites",favoritelistVideos);
-
+displayFromLocalStorage("watchlist", watchlistVideos);
+displayFromLocalStorage("favorites", favoritelistVideos);
 
 //Clear Watch List and Favorites List
 let watchlistTrash = document.getElementById("watchlist-trash");
 let favoriteslistTrash = document.getElementById("favoriteslist-trash");
-function clearList(str,obj,obj2,box,trash) {
-    let listObj = JSON.parse(localStorage.getItem(str));
-    trash.addEventListener("click",() => {
-        if(listObj.length!=0){
-            localStorage.setItem(str,JSON.stringify([]));
-            displayFromLocalStorage(str,box);
-            counterFun(str,obj,obj2);
-        };
-        });
+
+
+function clearList(localStorageKey) {
+  localStorage.setItem(localStorageKey,JSON.stringify([]));
+  localStorage.setItem("favorites_id",JSON.stringify([]));
+  if(localStorageKey=="favorites"){
+    favoritelistCounter.innerHTML = "0";
+    favoriteCounterHead.innerHTML = "0";
+    displayFromLocalStorage('favorites',favoritelistVideos);
+
+  }else {
+    watchlistCounterHead.innerHTML = "0";
+    watchlistCounter.innerHTML = "0";
+    displayFromLocalStorage("watchlist",watchlistVideos);
+  }
 }
-clearList("watchlist",watchlistCounter,watchlistCounterHead,watchlistVideos,watchlistTrash);
-clearList("favorites",favoritelistCounter,favoriteCounterHead,favoritelistVideos,favoriteslistTrash);
+
+watchlistTrash.addEventListener("click", () => {
+  clearList("watchlist");
+});
+
+favoriteslistTrash.addEventListener("click", () => {
+  clearList("favorites");
+});
+
 
 
